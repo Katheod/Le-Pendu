38763c9309937106575189f83de33f10ccf0f90c
Mathis Boivin <mathis.boivin.2@ulaval.ca>
2025-02-11 13:54:44 -0500

    new

--- Le jeu.py
+++ Le jeu.py
@@ -1,44 +1,25 @@
 import random
-# from Mots import mots
+from Mots import mots
 
-mots = ["banane"]
-def choisir_le_mot():
-    return random.choice(mots)
+def mot_valide(mots):
+    mot = random.choice(mots)
+    while "-" in mot or " " in mot:
+        mot = random.choice(mots)
 
-def deviner_le_mots(x):
-    tentative = str(input("Je pense que le mot est : ").lower())
-    if tentative == x:
-        print(f"Bravo! le mot caché était bien {x}!")
+    return mot.upper()
+
+def Le_pendu();
+    mot = mot_valide(mots)
+    lettres_mot = set(mot)
+    lettres_essayées = set()
+    joueur_essai = input("Essais une lettre : ").upper()
+    if joueur_essai.isalpha() and joueur_essai not in lettres_essayées:
+        lettres_essayées.add(joueur_essai)
+        if joueur_essai in lettres_mot:
+            lettres_essayées.remove(joueur_essai)
+    
+    elif joueur_essai in lettres_essayées:
+        print("Tu as déjà essayé cette lettre. Essaie une autre.")
+
     else:
-        print(f"Désolé, le mot caché n'est pas {tentative}.")
-        game()
-
-
-def Test_avec_lettre(x):
-    lettre = str(input("Quelle lettre veux-tu essayer ? ").lower())
-    réponse = ["_"] *len(x)
-    for c in range(len(list(x))):
-        if x[c] == lettre:
-            réponse[c] = lettre
-
-    réponse_str = "".join(réponse) 
-    
-    if réponse_str == x:
-        print(f"Bravo! Tu as trouvé la lettre manquante! Le mot était {x}")
-    else:
-        print("Tu as ça {réponse}")
-    
-def game():
-    mot = choisir_le_mot()
-    while True:
-        Décision = str(input("Veux-tu essayer deviner le mot (m) ou tester une lettre (l) ? ").lower())
-        if Décision == "m":
-            deviner_le_mots(mot)
-        elif Décision == "l":
-            Test_avec_lettre(mot)
-        else:
-            print(f"Veuillez choisir la lettre m pour deviner un mot ou l pour tenter une lettre")     
-
-
-game()
-    
+        print("Caractère invalide")
