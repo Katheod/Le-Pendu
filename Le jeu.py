55f590ceb0b857fd6e4f2e178f0af6c28a81e42b
Mathis Boivin <mathis.boivin.2@ulaval.ca>
2025-02-11 12:57:42 -0500

    MOdif again

--- Le jeu.py
+++ Le jeu.py
@@ -1,8 +1,19 @@
 import random
-from Mots import mots
+# from Mots import mots
 
+mots = ["banane"]
 def choisir_le_mot():
-    mot = random.choice(mots)
-    print(mot)
+    random.choice(mots)
 
-ch
+def deviner_le_mots(x):
+    tentative = str(input("Je pense que le mot est : ").lower())
+    if tentative == x:
+        print(f"Bravo! le mot caché était bien {x}!")
+    else:
+        print(f"Désolé, le mot caché n'est pas {tentative}.")
+    
+def game():
+    mot = choisir_le_mot()
+
+game()
+    
